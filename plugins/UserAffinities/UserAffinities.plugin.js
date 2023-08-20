fab16c08605a61ada7108f58005b0dabdf0f9590
Sarah <67713330+ItMeSarah@users.noreply.github.com>
2023-08-20 18:14:38 -0500

    Added self-install  for when users try to double click to install the plugin.
    Added self-install  for when users try to double click to install the plugin.

--- plugins/UserAffinities/UserAffinities.plugin.js
+++ plugins/UserAffinities/UserAffinities.plugin.js
@@ -7,6 +7,30 @@
  * @invite 7kzb9h27nR
  * @website https://itmesarah.github.io/
  */
+ 
+/*@cc_on
+@if (@_jscript)
+    
+    // Offer to self-install for clueless users that try to run this directly.
+    var shell = WScript.CreateObject("WScript.Shell");
+    var fs = new ActiveXObject("Scripting.FileSystemObject");
+    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+    var pathSelf = WScript.ScriptFullName;
+    // Put the user at ease by addressing them in the first person
+    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+    } else if (!fs.FolderExists(pathPlugins)) {
+        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+        fs.MoveFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)));
+        // Show the user where to put plugins in the future
+        shell.Exec("explorer " + pathPlugins);
+        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+    }
+    WScript.Quit();
+
+@else@*/
 
 /**
  * Finds a value, subobject, or array from a tree that matches a specific filter.
@@ -56,7 +80,6 @@ module.exports = class UserAffinities {
         
         .affinity-label {
             color: var(--text-normal);
-            font-weight: 700;
         }
         
         .affinity-container {
@@ -128,8 +151,8 @@ module.exports = class UserAffinities {
             affinityLabel.textContent = "Affinity Score: ";
 
             const affinityValue = document.createElement("span");
-            affinityValue .classList.add("affinity-value");
-            affinityValue .textContent = Math.round(affinity.affinity);
+            affinityValue.classList.add("affinity-value");
+            affinityValue.textContent = Math.round(affinity.affinity);
 
             const affinityWrap = document.createElement("div");
             affinityWrap.classList.add("affinity-container");
@@ -141,3 +164,5 @@ module.exports = class UserAffinities {
         }
     }
 }
+
+/*@end@*/
