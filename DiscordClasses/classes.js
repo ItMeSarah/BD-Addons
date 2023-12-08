8c412a0e2183bdc54cedd6aa28c49eb0c5fd56b8
Sarah <67713330+ItMeSarah@users.noreply.github.com>
2023-12-07 23:26:18 -0600

    Update classes.js

--- DiscordClasses/classes.js
+++ DiscordClasses/classes.js
@@ -5631,6 +5631,14 @@
             zoomin: "zoomin_fa2e9f"
         }
     },
+    898689: function(e, t, o) {
+        "use strict";
+        e.exports = {
+            container: "container_dc0bc1",
+            iconBackground: "iconBackground_c5c678",
+            link: "link__2ad53"
+        }
+    },
     416465: function(e, t, o) {
         "use strict";
         e.exports = {
@@ -5707,6 +5715,7 @@
             newItemTypeBadge: "newItemTypeBadge__7e6f7",
             page: "page_be1421",
             pageWrapper: "pageWrapper__582b9",
+            pageWrapperBackgroundPrimary: "pageWrapperBackgroundPrimary__74f14",
             pageWrapperFullscreen: "pageWrapperFullscreen__7f748",
             regularShopCard: "regularShopCard__06c2e",
             shop: "shop_b31ed2",
